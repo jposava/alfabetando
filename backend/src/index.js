b8b1be3b8c7b3130c9db238f757256fe3ce6a761
jposava <jposava@gmail.com>
2020-10-30 11:47:02 -0300

    modo antigo de iniciar o server sem express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const http = require('http');
+const host = '192.168.0.105';
+const port = 3000;
 
-const server = express();
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    res.end('Ola mundo \n meu primeiro script');
+});
 
-server.use(express.json());
-
-server.listen(3333, () => {
-    console.log('API ONLINE');
+server.listen(port, host, () => {
+    console.log(`Server rodando em http://${host}:${port}`);
 });
